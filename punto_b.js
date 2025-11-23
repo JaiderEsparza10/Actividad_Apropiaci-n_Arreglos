7c515113962ace15cf0dbcbad077bc124afd75cb
Jaider Esparza <xenno2003@gmail.com>
2025-11-23 10:31:37 -0500

    Se realizó la respuesta de las preguntas faltantes

--- punto_b.js
+++ punto_b.js
@@ -12,3 +12,11 @@ for (const verdura of verduras){
     console.log(verdura);
     
 }
+
+// c. En pareja, discutan y escriban una conclusión breve:
+// i. ¿Cuándo es más útil for?
+// Respuesta: El ciclo for es mas recomendable cuando necesita conocer y utilizar el índice posición del elemento dentro del bucle 
+// (por ejemplo, para modificar un elemento en esa posición, o para iterar en sentido inverso).
+// ii. ¿Cuándo es más útil for...of?
+// Respuesta: Es más util cuando Solo te interesa el valor del elemento y quieres un código 
+// más claro y conciso para leer los datos de un array o de cualquier objeto iterable.
