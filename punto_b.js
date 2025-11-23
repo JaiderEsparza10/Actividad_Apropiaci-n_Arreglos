20b838cce6e519daf9dd9add49a48e9fe151c763
Jaider Esparza <xenno2003@gmail.com>
2025-11-23 10:27:41 -0500

    Terminamos el ejercicio b con sus debidos comentarios

--- punto_b.js
+++ punto_b.js
@@ -1,1 +1,14 @@
-punto_b.js
+// UTILIZAMOS EL ARREGLO QUE QUEDÓ DEL EJERCICIO ANTERIOR
+let verduras = ["Zanahoria" , "Espinacas" , "pimenton" , "Cebolla"];
+console.log("---- FOR ----");
+// 1. AHORA CREAMOS EL CICLO FOR PARA RECORRER EL ARREGLO POR INDICE Y VALORA
+for (let i = 0; i < verduras.length; i++){
+    console.log("Posición [" + i + "] Verdura: " ,verduras[i]);
+}
+console.log("---- FOR OF ----");
+
+// 2.AHORA UTILIZAMOS EL ´CICLO FOR OF´ PARA MOSTRAR SOLO VALORES
+for (const verdura of verduras){
+    console.log(verdura);
+    
+}
